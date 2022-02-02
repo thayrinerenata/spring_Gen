f3595dc0c813a275cdb6d2badd0fb40aadfbd46e
Thayrine <thayrinerenata@gmail.com>
2022-02-01 23:35:16 -0300

    entendendo melhor a criação de uma aplicação Spring

--- src/main/java/com/helloword/hello/controller/HelloController.java
+++ src/main/java/com/helloword/hello/controller/HelloController.java
@@ -1,21 +1,26 @@
 package com.helloword.hello.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RestMapping ("/helloword")
-
+@RequestMapping("/hello")
 public class HelloController {
 	
-	@GetMapping ("/exercicio1")
+	@GetMapping("/exercicio1")
 	public String Hello() {
 		return "raiva";
 	}
 	
-	@GetMapping ("/exercicio2")
+	@GetMapping("/exercicio2")
 	public String Hay() {
 		return "sangue nos olhos";
+	}
+	
+	@GetMapping("/exercicio3")
+	public String Hola() {
+		return "mais um pouco eu surto";
 	}
 
 }
